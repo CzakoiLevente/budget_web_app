eae73a67cfd66baa47a962d37211023e1cfbb151
CzakoiLevente <czakoilevente@gmail.com>
2020-08-19 12:17:50 +0200

    modified for first setup

--- backend/create-db.sql
+++ backend/create-db.sql
@@ -1,23 +1,20 @@
-CREATE TABLE IF NOT EXISTS `test` (
-  `item-id` int (10) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `quantity` int (10),
-  `price` int (10),
-  `item` VARCHAR (200) NOT NULL
-);
+CREATE DATABASE test_project;
 
+USE test_project;
 
 CREATE TABLE IF NOT EXISTS `test` (
   `item-id` int (10) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `quantity` int (10) NOT NULL,
   `price` int (10) NOT NULL,
-  `item` VARCHAR (200) NOT NULL,
+  `item` VARCHAR (200) CHARACTER SET utf8 NOT NULL,
   `timestamp` TIMESTAMP,
   `shop` VARCHAR (30) NOT NULL,
   `payment_method` VARCHAR (30) NOT NULL,
   `currency` VARCHAR (5) NOT NULL,
-  `date` DATE
+  `date` DATE NOT NULL
 );
 
+/*
 INSERT INTO test (quantity, price, item) VALUES (1,1,'asd');
 
 DELETE FROM test ORDER BY `item-id` DESC LIMIT 1;
@@ -27,3 +24,4 @@ select * from test order by `item-id` desc limit 1;
 ALTER TABLE test MODIFY item CHAR(50) CHARACTER SET utf8;
 
 ALTER TABLE test ADD date DATE;
+*/
