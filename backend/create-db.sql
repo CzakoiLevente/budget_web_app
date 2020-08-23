61ade1759fd4ada283ec23689f2909ca342909d4
CzakoiLevente <czakoilevente@gmail.com>
2020-08-23 23:57:38 +0200

    added data to 'date', column default value set to NOT NULL

--- backend/create-db.sql
+++ backend/create-db.sql
@@ -11,17 +11,17 @@ CREATE TABLE IF NOT EXISTS `test` (
   `shop` VARCHAR (30) NOT NULL,
   `payment_method` VARCHAR (30) NOT NULL,
   `currency` VARCHAR (5) NOT NULL,
-  `date` DATE NULL
+  `date` DATETIME NOT NULL
 );
 
-INSERT INTO test (quantity, price, item, shop, payment_method, currency) 
-VALUES (1,1,'apple', 'LIDL', 'cash' , '€'),
-(3,100000,'tesla model S', 'TESLA Zürich', 'transfer' , '€'),
-(2,20,'pear', 'market', 'cash' , 'Ft'),
-(3,5,'gas', 'SHELL', 'crypto transfer' , 'BTC'),
-(10,1.5,'bucket', 'PRAKTIKER', 'cash' , 'ECU'),
-(1,10000,'mobile phone', 't-mobile', 'card' , 'CZSK'),
-(2,43.65,'telephone bill', 'VODAFONE', 'other' , '€');
+INSERT INTO test (quantity, price, item, shop, payment_method, currency, date) 
+VALUES (1,1,'apple','LIDL','cash','€','2020-08-10 12:44:26'),
+(3,100000,'tesla model S','TESLA Zürich','transfer','€','2020-06-23 08:32:02'),
+(2,20,'pear','market','cash','Ft','2020-06-23 09:12:34'),
+(3,5,'gas','SHELL','crypto transfer','BTC','2020-06-23 11:34:53'),
+(10,1.5,'bucket','PRAKTIKER','cash','ECU','2020-06-23 06:41:07'),
+(1,10000,'mobile phone','t-mobile','card','CZSK','2020-06-23 14:13:17'),
+(2,43.65,'telephone bill','VODAFONE','other','€','2020-06-23 18:55:49');
 
 /*
 INSERT INTO test (quantity, price, item) VALUES (1,1,'asd');
